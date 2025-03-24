c5133f3b7a42619973f340bbe087436e226f0cdf
danizalm05 <danizalm05@gmail.com>
2025-03-24 15:12:38 +0200

    13128

---
+++ gitNewLocalRepo.bat
@@ -0,0 +1,15 @@
+cls
+ 
+@echo off
+del .git 
+pause
+git init
+
+pause
+git remote add origin https://github.com/danizalm05/linux01.git
+
+git remote -v
+pause 
+cls
+git pull   origin master
+pause
