22fe20e4d10fc2de77562c4d643f96b9b8a20c0e
Your Name <you@example.com>
2025-04-01 22:56:19 +0300

    wi

---
+++ mogli/wifiInit.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+import os
+import subprocess
+from subprocess import check_call
+
+
+
+
+print("\nEnter the interface:(Default >> (wlan0mon/wlan1mon))")
+order = "airodump-ng wlan0"
+ 
+print("When Done Press CTRL+c")
+cmd = os.system("sleep 1")
+geny  = os.system(order)
+cmd = os.system("sleep 1")
+
+
+       
+
+
+
