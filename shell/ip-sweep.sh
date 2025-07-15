50f9bcaf82e16796a9dd891a7401160e9d634517
danizalm05 <danizalm05@gmail.com>
2025-07-15 17:23:20 +0300

    26217

--- shell/ip-sweep.sh
+++ shell/ip-sweep.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 addr = 10.0.2.15
-for ip in `seq 1 254`; do
-    ping -c 1 $addr.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
+
+
+
+if [ "$1" == "" ]
+then
+echo "You forgot an IP address!"
+echo "Syntax: ./ip-sweep.sh 10.0.2"
+
+else
+  for ip in `seq 1 254`; do
+    ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
   done
+fi
